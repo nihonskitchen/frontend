941a6ece331e44b657228dd1ee99851eeff638ee
LaShawn Toyoda <lts001@gmail.com>
2021-01-14 00:12:19 +0900

    implement firebase auth

--- pages/login.vue
+++ pages/login.vue
@@ -2,14 +2,24 @@
   <div class="container">
     <div class="center-div">
       <div class="form-card">
-        <form action="submit">
+        <form action="submit" method="POST" @submit.prevent="pressed">
           <h2>Login</h2>
           <br />
-          <input type="email" placeholder="email address" />
+          <input
+            type="email"
+            placeholder="email address"
+            v-model="email"
+            required
+          />
           <br />
-          <input type="password" placeholder="password" />
+          <input
+            type="password"
+            placeholder="password"
+            required
+            v-model="password"
+          />
           <br />
-          <button class="large-btn">Login</button>
+          <button type="submit" class="large-btn">Login</button>
         </form>
         Don't have an account? <nuxt-link to="/signup">Sign up!</nuxt-link>
       </div>
@@ -18,11 +28,37 @@
 </template>
 
 <script>
-export default {};
+import firebase from "firebase/app";
+import "firebase/auth";
+
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      errors: "",
+    };
+  },
+  methods: {
+    pressed() {
+      alert("Logged in");
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then((user) => {
+          console.log(user);
+          
+          this.$router.push("/");
+        })
+        .catch((error) => {
+          this.errors = error;
+        });
+    },
+  },
+};
 </script>
 
 <style>
-
 input {
   width: 100%;
   padding: 5px;
