2734962aaa4d1b1762af172943c71c77d7d6c1a7
LaShawn <lashawn@LaShawns-MacBook-Pro.local>
2021-01-22 17:48:07 +0900

    add password requirements

--- pages/user/profile.vue
+++ pages/user/profile.vue
@@ -1,21 +1,43 @@
 <template>
   <div class="info-card">
-    <div>
-      <h1>User Profile</h1>
-    </div>
-    <div>
-      <label for="user-email">Email address: (Non-changeable)</label>
-      <input id="user-email" type="text" :value="this.account.email" readonly>
-    </div>
-    <div>
-      <label for="" >Reset Password:</label>
-      <input id="current-password" type="text" placeholder="Current password">
-      <input id="new-password" type="text" placeholder="Enter new password">
-      <input id="confirm-new-password" type="text" placeholder="Confirm new password">
-    </div>
-    <div>
-      <button class="submit-btn margins">Submit changes</button>
-    </div>
+    <form action="submit" method="POST" @submit.prevent>
+      <div>
+        <h1>User Profile</h1>
+      </div>
+      <div>
+        <label for="user-email">Email address: (Non-changeable)</label>
+        <input
+          id="user-email"
+          type="text"
+          :value="this.account"
+          readonly
+        />
+      </div>
+      <div>
+        <label for="">Reset Password:</label>
+        <input
+        @input="checkPasswordLength"
+          id="new-password"
+          type="password"
+          placeholder="Enter new password"
+          v-model="newPassword"
+          required
+        />
+        <input
+        @input="comparePasswords"
+          id="confirm-new-password"
+          type="password"
+          placeholder="Confirm new password"
+          v-model="newPasswordAgain"
+          required
+        />
+      </div>
+      <div v-if="samePassword === false">Passwords don't match</div>
+      <div v-if="passwordMinChars === false">Password too short</div>
+      <div>
+        <button class="submit-btn margins">Submit changes</button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -30,12 +52,23 @@ export default {
   data() {
     return {
       account: "",
+      confirmPassword: "",
+      newPassword: "",
+      newPasswordAgain: "",
+      samePassword: false,
+      passwordMinChars: null,
     };
   },
   methods: {
     getUserData() {
-      this.account = this.$store.state.users.user;
+      this.account = this.$store.state.users.user.email;
       console.log(this.account);
+    },
+        comparePasswords() {
+      this.samePassword = this.newPassword === this.newPasswordAgain;
+    },
+    checkPasswordLength() {
+      this.passwordMinChars = this.newPassword.length >= 6;
     },
   },
 };
