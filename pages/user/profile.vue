fd1e8242797dfd38ac2f0887c9b8de93a1768cc7
LaShawn <lashawn@LaShawns-MacBook-Pro.local>
2021-01-22 17:30:12 +0900

    create profile card

--- pages/user/profile.vue
+++ pages/user/profile.vue
@@ -1,15 +1,45 @@
 <template>
-  <div class="container center-div">
+  <div class="info-card">
+    <div>
       <h1>User Profile</h1>
+    </div>
+    <div>
+      <label for="user-email">Email address: (Non-changeable)</label>
+      <input id="user-email" type="text" :value="this.account.email" readonly>
+    </div>
+    <div>
+      <label for="" >Reset Password:</label>
+      <input id="current-password" type="text" placeholder="Current password">
+      <input id="new-password" type="text" placeholder="Enter new password">
+      <input id="confirm-new-password" type="text" placeholder="Confirm new password">
+    </div>
+    <div>
+      <button class="submit-btn margins">Submit changes</button>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import firebase from "firebase/app";
+import "firebase/auth";
 
-}
+export default {
+  mounted() {
+    this.getUserData();
+  },
+  data() {
+    return {
+      account: "",
+    };
+  },
+  methods: {
+    getUserData() {
+      this.account = this.$store.state.users.user;
+      console.log(this.account);
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
