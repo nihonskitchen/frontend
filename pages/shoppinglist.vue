24da39d0ecfef3cca6be1eeed88723b75d62f0a0
SayakaNakajima <andsayaka@gmail.com>
2021-01-23 22:41:04 +0900

    build remove unwanted lines from shopping list

--- pages/shoppinglist.vue
+++ pages/shoppinglist.vue
@@ -36,62 +36,10 @@ export default {
       itemList: this.$store.state.shoppinglist.shoppingList
     }
   },
-  //mountedか何かに変更する必要ありそう
-  // async asyncData({ $axios, store }) {
-  //     console.log(store.state.shoppinglist.shoppingList);
-  //     let lists = store.state.shoppinglist.shoppingList;
-  //     const apiUrl = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?applicationId=1060380614247351390&sort=standard&genreId=100227&elements=itemUrl,itemName,itemPrice&availability=1&hits=1&keyword="
-  //     for (let i = 0; i < lists.length; i++) {
-  //       let getURL = apiUrl + lists[i];
-  //       console.log(getURL);
-  //       let item = await $axios.$get(getURL);
-  //       console.log(item);
-  //       lists[i].url = item.Items.Item.itemUrl
-  //     }
-  //     console.log(lists);
-  //     store.commit("shoppinglist/setLists", lists);
-  // },
-  // 
   methods: {
     rakutenURL (url) {
       window.open(`https://search.rakuten.co.jp/search/mall/${url}/100227/?st=O`, '_blank')
     }
-    // async amazonFresh () {
-    //   const amazonFreshJson = {
-    //     ingredients: [],
-    //     saved: false,
-    //     recipeComposition: {
-    //       saved: false
-    //     }
-    //   };
-    //   const listItems = this.$store.state.shoppinglist.shoppingList;
-    //   for (const i of listItems) {
-    //     const ingredient = {
-    //       name: "", componentIndex: 0,
-    //       quantityList: [ { unit: "", amount: "" } ]
-    //     };
-    //     ingredient.name = i.ingredient_name;
-    //     ingredient.quantityList[0].amount = i.amount;
-    //     ingredient.quantityList[0].units = i.units;
-    //     amazonFreshJson.ingredients.push(ingredient);
-    //   };
-    //   // console.log(amazonFreshJson.ingredients);
-    //   // console.log(JSON.stringify(amazonFreshJson));
-
-    //   // JSONで送ってるけど415が返ってくる
-    //   // いまいち何を修正したらいいか不明
-    //   // 参考：http://limedaley.com/post/amazon-fresh-api
-    //   // formデータ作って送信する挙動をかますとうまくいく？かも？
-    //   await this.$axios.$post(
-    //     "https://www.amazon.co.jp/afx/ingredients/verify",
-    //     JSON.stringify(amazonFreshJson),
-    //     { 
-    //       header : { 'Content-Type': 'application/json;charset=UTF-8' }
-    //     }
-    //   );
-
-    // },
-
   }
 };
 </script>
@@ -135,7 +83,8 @@ button {
   background-color: #e76c73;
   border: 0px;
   font-size: 15px;
-  border-radius: 0px;
+  border-radius: 8px;
+  height: 20px
 }
 .flexbox {
   display: flex;
