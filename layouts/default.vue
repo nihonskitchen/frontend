7405d634efade2470a7ee2eddf89f3bb0c558fd6
LaShawn <lashawn@LaShawns-MacBook-Pro.local>
2021-01-24 14:57:40 +0900

    refactor signup

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,9 +13,18 @@
   box-sizing: border-box;
   font-family: sans-serif;
 }
+a {
+  text-decoration: none;
+}
 body {
   background-color: #f4f2ee;
   color: #142C28;
+}
+p {
+  margin-bottom: 15px;
+}
+p:last-of-type {
+  margin-bottom: 0px;
 }
 .center-div {
   min-height: 90vh;
@@ -25,20 +34,6 @@ body {
 .container {
   min-height: 90vh;
   width: 100%;
-}
-a {
-  text-decoration: none;
-}
-p {
-  margin-bottom: 15px;
-}
-p:last-of-type {
-  margin-bottom: 0px;
-}
-form {
-  position: relative;
-  justify-content: space-evenly;
-  align-content: center;
 }
 .form-card {
   position: relative;
@@ -50,6 +45,35 @@ form {
   border-radius: 8px;
   min-width: 360px;
   max-width: 360px;
+}
+input {
+  width: 100%;
+  padding: 5px;
+  margin: 5px;
+}
+label {
+  font-size: 12px;
+}
+.large-btn {
+  padding: 5px;
+  width: 100%;
+  margin: 5px;
+  background-color: #F7B981;
+  border: 0px;
+  font-size: 15px;
+  border-radius: 8px;
+  color: white;
+}
+.large-btn:hover {
+  padding: 5px;
+  width: 100%;
+  margin: 5px;
+  background-color: #ffa95d;
+  border: 0px;
+  font-size: 15px;
+  border-radius: 8px;
+  color: white;
+  cursor: pointer;
 }
 #main-div {
   display: inherit;
